6d10ca5adbf6fafa33c67cf2685841223def777d
myzz66 <111197543+myzz66@users.noreply.github.com>
2022-09-24 20:33:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// var swiper = new Swiper(".mySwiper", {
+//   //   slidesPerView: 1,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+
+var swiper = new Swiper(".mySwiper", {
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     type: "progressbar",
+  //   },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
